007c7ef715c2ca67d0fbf207d2a0cf755f1d50e3
Jose Maria Roman <jose.maria.roman87@gmail.com>
2023-04-12 19:35:09 +0200

    [crud_posts] add functionality to create edit and delete Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_063656) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_170530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "content"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    name { "MyString" }
+    title { "MyString" }
+    content { "MyText" }
+    published_at { "2023-04-12 19:05:30" }
+  end
+end
